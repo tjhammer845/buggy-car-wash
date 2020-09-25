d4cd2c39a52fb9b0e39f4e74886831ef9069882b
Thomas Hammer <tjhammer845@gmail.com>
2020-09-24 22:31:20 -0400

    mailer

--- contact.php
+++ contact.php
@@ -1,131 +1,65 @@
 <?php
-/*
-THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
-*/
 
-require './vendor/php/PHPMailer-master/PHPMailerAutoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+require __DIR__ . '/vendor/autoload.php';
 
-/*
-*  CONFIGURE EVERYTHING HERE
-*/
-*
-*  CONFIGURE EVERYTHING HERE
-*/
+$errors = [];
+$errorMessage = '';
 
-// an email address that will be in the From field of the email.
-$fromEmail = 'team@buggycarwash.com';
-$fromName = 'Buggy Car Wash';
+if (!empty($_POST)) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-// an email address that will receive the email with the output of the form
-$sendToEmail = 'tjhammer845@gmail.com';
-$sendToName = 'Demo contact form';
-
-// subject of the email
-$subject = 'New message from contact form';
-
-// smtp credentials and server
-
-$smtpHost = 'smtp.buggycarwash.com';
-$smtpUsername = 'team@buggycarwash.com';
-$smtpPassword = 'Bigfrank18?';
-
-// form field names and their translations.
-// array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
-
-// message that will be displayed when everything is OK :)
-$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
-
-// If something goes wrong, we will display this message.
-$errorMessage = 'There was an error while submitting the form. Please try again later';
-
-/*
-*  LET'S DO THE SENDING
-*/
-
-// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
-error_reporting(E_ALL & ~E_NOTICE);
-
-try {
-    if (count($_POST) == 0) {
-        throw new \Exception('Form is empty');
+    if (empty($name)) {
+        $errors[] = 'Name is empty';
     }
-    
-    $emailTextHtml = "<h1>You have a new message from your contact form</h1><hr>";
-    $emailTextHtml .= "<table>";
-    
-    foreach ($_POST as $key => $value) {
-        // If the field exists in the $fields array, include it in the email
-        if (isset($fields[$key])) {
-            $emailTextHtml .= "<tr><th>$fields[$key]</th><td>$value</td></tr>";
+
+    if (empty($email)) {
+        $errors[] = 'Email is empty';
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'Email is invalid';
+    }
+
+    if (empty($message)) {
+        $errors[] = 'Message is empty';
+    }
+
+
+    if (!empty($errors)) {
+        $allErrors = join('<br/>', $errors);
+        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+    } else {
+        $mail = new PHPMailer();
+
+        // specify SMTP credentials
+        // $mail->isSMTP();
+        $mail->Host = 'smtp.bluehost.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'team@buggycarwash.com';
+        $mail->Password = 'Bigfrank18?';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 2525;
+
+        $mail->setFrom($email, 'Buggy Car Wash Website');
+        $mail->addAddress('tjhammer845@gmail.com', 'Me');
+        $mail->Subject = 'New message from the Buggy website';
+
+        // Enable HTML if needed
+        $mail->isHTML(true);
+
+        $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", nl2br($message)];
+        $body = join('<br />', $bodyParagraphs);
+        $mail->Body = $body;
+
+        echo $body;
+        if($mail->send()){
+
+            header('Location: thank-you.html'); // redirect to 'thank you' page
+        } else {
+            $errorMessage = 'Oops, something went wrong. Mailer Error: ' . $mail->ErrorInfo;
         }
     }
-    $emailTextHtml .= "</table><hr>";
-    $emailTextHtml .= "<p>Have a nice day,<br>Best,<br>Ondrej</p>";
-    
-    $mail = new PHPMailer;
-    
-    $mail->setFrom($fromEmail, $fromName);
-    $mail->addAddress($sendToEmail, $sendToName); // you can add more addresses by simply adding another line with $mail->addAddress();
-    $mail->addReplyTo($from);
-    
-    $mail->isHTML(true);
-    
-    $mail->Subject = $subject;
-    $mail->Body    = $emailTextHtml;
-    $mail->msgHTML($emailTextHtml); // this will also create a plain-text version of the HTML email, very handy
-    
-    
-    $mail->isSMTP();
-    
-    //Enable SMTP debugging
-    // 0 = off (for production use)
-    // 1 = client messages
-    // 2 = client and server messages
-    $mail->SMTPDebug = 0;
-    $mail->Debugoutput = 'html';
-    
-    //Set the hostname of the mail server
-    // use
-    // $mail->Host = gethostbyname('smtp.gmail.com');
-    // if your network does not support SMTP over IPv6
-    $mail->Host = gethostbyname($smtpHost);
-    
-    //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-    $mail->Port = 587;
-    
-    //Set the encryption system to use - ssl (deprecated) or tls
-    $mail->SMTPSecure = 'tls';
-    
-    //Whether to use SMTP authentication
-    $mail->SMTPAuth = true;
-    
-    //Username to use for SMTP authentication - use full email address for gmail
-    $mail->Username = $smtpHost;
-    
-    //Password to use for SMTP authentication
-    $mail->Password = $smtpPassword;
-    
-    if (!$mail->send()) {
-        throw new \Exception('I could not send the email.' . $mail->ErrorInfo);
-    }
-    
-    $responseArray = array('type' => 'success', 'message' => $okMessage);
-} catch (\Exception $e) {
-    // $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-    $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
 }
 
-
-// if requested by AJAX request return JSON response
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-    $encoded = json_encode($responseArray);
-    
-    header('Content-Type: application/json');
-    
-    echo $encoded;
-}
-// else just display the message
-else {
-    echo $responseArray['message'];
-}
+?>
